471b85468660ffed6b0821b024c30809ac467ca5
Guilherme R Galanti <galanti_imp@hotmail.com>
2022-09-15 11:14:40 -0300

    concluindo ResultadoActivity.kt

--- app/src/main/java/com/guilhermegalanti/introduoocean/MainActivity.kt
+++ app/src/main/java/com/guilhermegalanti/introduoocean/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.guilhermegalanti.introduoocean
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,12 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var botaoMeAperte: Button? = null
+        var botaoNovaTela: Button? = null
         var tvPrincipal: TextView? = null
         var etName: EditText? = null
 
         var contador = 0
 
         botaoMeAperte = findViewById(R.id.botao_me_aperte)
+        botaoNovaTela = findViewById(R.id.btnNovaTela)
         tvPrincipal = findViewById(R.id.tvPrincipal)
         etName = findViewById(R.id.etNome)
 
@@ -30,8 +33,14 @@ class MainActivity : AppCompatActivity() {
                 contador++
                 Toast.makeText(this, "Ai que delícia!", Toast.LENGTH_LONG)
                     .show()
-                tvPrincipal?.text = etName.text?.toString()
-            } else etName.error = "Digite algo válido!"
+                tvPrincipal?.text = etName?.text?.toString()
+            } else etName?.error = "Digite algo válido!"
+        }
+
+        botaoNovaTela?.setOnClickListener {
+            val intent = Intent(this, ResultadoActivity::class.java)
+            intent.putExtra("Texto", etName?.text?.toString())
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/guilhermegalanti/introduoocean/ResultadoActivity.kt
+++ app/src/main/java/com/guilhermegalanti/introduoocean/ResultadoActivity.kt
@@ -1,11 +1,28 @@
 package com.guilhermegalanti.introduoocean
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        var btnVoltar: Button? = null
+        var tvResultado: TextView? = null
+
+        val texto = intent.getStringExtra("Texto")
+
+        btnVoltar = findViewById(R.id.btn_voltar)
+        tvResultado = findViewById(R.id.tvResultado)
+
+        tvResultado?.text = texto
+
+        btnVoltar?.setOnClickListener {
+            finish()
+        }
     }
 }
